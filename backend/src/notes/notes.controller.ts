920bd982ff79a25f95d1749ad7d4acb07a92b685
William Ravel <wravel@gmail.com>
2021-02-27 21:21:32 +0100

    Fix typo in path

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -8,7 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { PaginationQueryDto } from './../common/dto/pagination-query.dto';
 import { CreateNoteDto } from './create-note.dto';
 import { NotesService } from './notes.service';
 import { UpdateNoteDto } from './update-note.dto';
@@ -31,7 +31,7 @@ export class NotesController {
   create(@Body() createNoteDto: CreateNoteDto) {
     return this.notesService.create(createNoteDto);
   }
-
+  
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateNoteDto: UpdateNoteDto) {
     return this.notesService.update(id, updateNoteDto);
@@ -41,4 +41,5 @@ export class NotesController {
   delete(@Param('id') id: number) {
     return this.notesService.remove(id);
   }
+  
 }
