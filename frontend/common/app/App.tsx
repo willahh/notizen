595e56ec158b9202b466acd688527eb25353e427
William Ravel <wravel@gmail.com>
2021-03-01 22:17:32 +0100

    Add Auth and NoteLit views

--- frontend/common/app/App.tsx
+++ frontend/common/app/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Note } from '@notizen/frontend-common/features/note/Note';
 import { NoteThumb } from '@notizen/frontend-common/features/note/NoteThumb';
+import { NoteLit } from '@notizen/frontend-common/features/note/NoteLit';
+import { Auth } from '@notizen/frontend-common/features/auth/Auth';
 import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -15,19 +17,31 @@ function App() {
             <Link to="/note">note</Link>
           </li>
           <li>
-            <Link to="/login">login</Link>
+            <Link to="/note-thumb">note note-thumb</Link>
+          </li>
+          <li>
+            <Link to="/note-lit">note note-lit</Link>
+          </li>
+          <li>
+            <Link to="/auth">Auth</Link>
           </li>
         </ul>
       </nav> */}
       <Switch>
+        {/* <Route path="/">
+          <Auth />
+        </Route> */}
+        <Route path="/auth">
+          <Auth />
+        </Route>
         <Route path="/note">
           <Note />
         </Route>
-        <Route path="/login">
-          <div>LOGIN</div>
-        </Route>
-        <Route path="/">
+        <Route path="/note-thumb">
           <NoteThumb />
+        </Route>
+        <Route path="/note-lit">
+          <NoteLit />
         </Route>
       </Switch>
     </>
