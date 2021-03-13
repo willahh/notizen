cba97b259a839a9227b910839f1c527fa90bb69f
William Ravel <wravel@gmail.com>
2021-03-13 18:10:59 +0100

    Add todos

--- backend/src/notes/note-enum.ts
+++ backend/src/notes/note-enum.ts
@@ -1,10 +1,12 @@
 // TODO: Share with frontend
+// TODO: Enum convention, key in Camelcase
 export enum TagIcon {
   TAG = 'TAG',
   HASHTAG = 'HASHTAG',
 }
 
 // TODO: Share with frontend
+// TODO: Enum convention, key in Camelcase
 export enum NoteColor {
   GRAY = 'GRAY',
   RED = 'RED',
