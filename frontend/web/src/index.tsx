c846f8e0a449b4ff33b929aba9dae62f9ef81fc0
William Ravel <wravel@gmail.com>
2021-03-13 02:08:55 +0100

    Setup index.html for mobile / Disable serviceWorker

--- frontend/web/src/index.tsx
+++ frontend/web/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { store } from '@notizen/frontend-common/src/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker.js';
+// import * as serviceWorker from './serviceWorker.js';
 import '@notizen/frontend-common/index.css';
 import App from '@notizen/frontend-common/src/App';
 
@@ -25,4 +25,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
